ba1e27fd9145efc541fc746919a6a1666df41218
Matei Copot <matei@copot.eu>
2017-09-09 12:57:08 +0200

    hooked vue so it responds to multiple paths

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,22 @@ const env = require('./../.env');
 const db = require('./db');
 const log = (message, type) => console.log(new Date, message);
 
+
 api.get('/', (req, res) => {
   res.send(Math.random() + ' hello')
 })
 
 app.use('/api', api);
+
+const vuePaths = [
+  '/',
+  '/login',
+  '/@:user'
+];
+
+app.get(vuePaths, (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets/index.html'));
+});
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.listen(env.port, () => {
