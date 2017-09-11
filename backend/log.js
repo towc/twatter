e9a7dd69b2236043c7ed6a4bf8eb134e392e7e7c
Matei Copot <matei@copot.eu>
2017-09-11 23:24:55 +0200

    slightly expanded logger

--- backend/log.js
+++ backend/log.js
@@ -1,1 +1,13 @@
-module.exports = (message, type) => console.log(new Date, message);
+const [ DATE, TYPE, MESSAGE ] = [ 0, 1, 2 ];
+
+module.exports = (message, { type }) => {
+  const args = [new Date, 'LOG', message];
+
+  if(type === 'error') {
+    args[TYPE] = 'ERROR\n';
+    return console.error.apply(args);
+  }
+
+  console.log.apply(args);
+ 
+}
